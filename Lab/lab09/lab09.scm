599c834108331f07826d12672b848afae53ea397
231250160-Zuo Tao <3105327090@qq.com>
2025-02-14 20:47:01 +0800

    lab09

--- Lab/lab09/lab09.scm
+++ Lab/lab09/lab09.scm
@@ -1,10 +1,20 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2)
+  (if (> num1 num2)
+    1
+    (if (< num1 num2)
+      -1
+      0)))
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num)
+  (lambda (x) (+ x num)))
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g) 
+  (lambda (x) (f (g x))))
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+(define (repeat f n)
+  (if (= n 0)
+    (lambda (x) x)
+    (composed f (repeat f (- n 1)))))
 
 (define (max a b)
   (if (> a b)
@@ -16,4 +26,7 @@
       b
       a))
 
-(define (gcd a b) 'YOUR-CODE-HERE)
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (remainder a b))))
