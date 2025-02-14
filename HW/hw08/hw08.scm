3e53dd69e49dcd728bf60d168b9672cb7145aed3
231250160-Zuo Tao <3105327090@qq.com>
2025-02-14 21:28:14 +0800

    hw08

--- HW/hw08/hw08.scm
+++ HW/hw08/hw08.scm
@@ -1,7 +1,27 @@
-(define (ascending? s) 'YOUR-CODE-HERE)
+(define (cadr s) (car (cdr s)))
 
-(define (my-filter pred s) 'YOUR-CODE-HERE)
+(define (ascending? s) 
+  (if (null? s)
+      #t
+      (if (null? (cdr s))
+          #t
+          (if (<= (car s) (cadr s))
+              (ascending? (cdr s))
+              #f))))
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (my-filter pred s) 
+  (if (null? s)
+      '()
+      (if (pred (car s))
+          (cons (car s) (my-filter pred (cdr s)))
+          (my-filter pred (cdr s)))))
 
-(define (no-repeats s) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2) 
+  (if (null? lst1)
+      lst2
+      (cons (car lst1) (interleave lst2 (cdr lst1)))))
+
+(define (no-repeats s) 
+  (if (null? s)
+      '()
+      (cons (car s) (my-filter (lambda (x) (not (= x (car s)))) (no-repeats (cdr s))))))
