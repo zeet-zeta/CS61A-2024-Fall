0d2aeacc2663c8111174d62393bdb3a93fe99dc1
zeet <3105327090@qq.com>
2025-04-12 12:02:30 +0800

    lab11

--- Lab/lab11/lab11.scm
+++ Lab/lab11/lab11.scm
@@ -1,15 +1,22 @@
 (define (if-program condition if-true if-false)
-  'YOUR-CODE-HERE)
+  (list 'if condition if-true if-false))
 
 (define (square n) (* n n))
 
-(define (pow-expr base exp) 'YOUR-CODE-HERE)
+(define (pow-expr base exp) 
+  (cond
+    ((= exp 0) 1)
+    ((= exp 1) (list '* base 1))
+    ((even? exp) (list 'square (pow-expr base (/ exp 2))))
+    (else (list '* base (pow-expr base (- exp 1))))))
 
 (define-macro (repeat n expr)
-  `(repeated-call ,n ___))
+  `(repeated-call ,n (lambda () ,expr)))
 
 ; Call zero-argument procedure f n times and return the final result.
 (define (repeated-call n f)
   (if (= n 1)
-      ___
-      (begin ___ ___)))
+      (f)
+      (begin 
+        (f)
+        (repeated-call (- n 1) f))))
