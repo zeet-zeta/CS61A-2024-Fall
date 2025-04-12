edb143ab03c1cdfca65c41ae9f9aef4a9e442354
zeet <3105327090@qq.com>
2025-04-12 15:49:12 +0800

    hw09

--- HW/hw09/hw09.scm
+++ HW/hw09/hw09.scm
@@ -1,13 +1,18 @@
-(define (curry-cook formals body) 'YOUR-CODE-HERE)
+(define (curry-cook formals body)
+  (if (null? (cdr formals))
+    `(lambda (,(car formals)) ,body)
+    `(lambda (,(car formals)) ,(curry-cook (cdr formals) body))))
 
 (define (curry-consume curry args)
-  'YOUR-CODE-HERE)
+  (cond 
+    ((null? args) curry)
+    (else (curry-consume (curry (car args)) (cdr args)))))
 
 (define-macro (switch expr options)
   (switch-to-cond (list 'switch expr options)))
 
 (define (switch-to-cond switch-expr)
-  (cons _________
+  (cons 'cond
         (map (lambda (option)
-               (cons _______________ (cdr option)))
+               (cons (list 'equal? (car (cdr switch-expr)) (car option)) (cdr option)))
              (car (cdr (cdr switch-expr))))))
